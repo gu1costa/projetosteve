c983351e69657253f9d8a38a49232b6b97d51969
gu1costa <guilhermefecone@gmail.com>
2023-05-07 19:12:11 -0300

    Update projetosteve.py

--- projetosteve.py
+++ projetosteve.py
@@ -24,7 +24,7 @@ stream.start_stream()
 
 
 while True:
-    data = stream.read(4000)
+    data = stream.read(10000)
     if len(data) == 0:
         break
     if rec.AcceptWaveform(data):
@@ -42,4 +42,3 @@ while True:
         
 
 
-stream = p.open(format=pyaudio.paInt16, channels=1, rate=44100, input=True, frames_per_buffer=4096)
