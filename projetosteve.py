ef75190f8ff7a3116a553a8fa336baf036fa06dc
gu1costa <guilhermefecone@gmail.com>
2023-05-07 18:48:43 -0300

    reprodução da fala

--- projetosteve.py
+++ projetosteve.py
@@ -1,7 +1,18 @@
-
 from vosk import Model, KaldiRecognizer
 import os
 import pyaudio
+import pyttsx3
+import json
+
+
+# Síntese da voz
+engine = pyttsx3.init()
+
+def speak(text):
+    engine.say(text)
+    engine.runAndWait()
+
+# Speech Recognition
 
 model = Model("model")
 rec = KaldiRecognizer(model, 16000)
@@ -11,13 +22,24 @@ p = pyaudio.PyAudio()
 stream = p.open(format=pyaudio.paInt16, channels=1, rate=16000, input=True, frames_per_buffer=8000)
 stream.start_stream()
 
+
 while True:
     data = stream.read(4000)
     if len(data) == 0:
         break
     if rec.AcceptWaveform(data):
-        print(rec.Result())
-    else:
-        print(rec.PartialResult())
+        # result is a string
+        result = rec.Result()
+        # convert it to a json/dictionary
+        result = json.loads(result)
 
-print(rec.FinalResult())
+        if result is not None:
+            text = result['text']
+
+            print(text)
+            speak(text)
+
+        
+
+
+stream = p.open(format=pyaudio.paInt16, channels=1, rate=44100, input=True, frames_per_buffer=4096)
