7eadc53f14bebdf806b1616caec2ebad8fc0b1d2
gu1costa <guilhermefecone@gmail.com>
2023-05-07 20:27:30 -0300

    correção do input overflowed

--- projetosteve.py
+++ projetosteve.py
@@ -20,13 +20,13 @@ rec = KaldiRecognizer(model, 16000)
 
 # Opens microphone for listening.
 p = pyaudio.PyAudio()
-stream = p.open(format=pyaudio.paInt16, channels=1, rate=16000, input=True, frames_per_buffer=8000)
+stream = p.open(format=pyaudio.paInt16, channels=1, rate=16000, input=True, frames_per_buffer=2048)
 stream.start_stream()
 
 
 #Loop do reconhecimento de fala
 while True:
-    data = stream.read(10000)
+    data = stream.read(2048)
     if len(data) == 0:
         break
     if rec.AcceptWaveform(data):
